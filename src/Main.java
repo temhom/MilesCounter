fa9fc30ea43094d37bad3fd444c7d253c56824d5
Artem Khomutskikh <artem7700@mail.ru>
2022-12-17 15:30:49 +0300

    ignore_and_all

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+int a = 8800; // Стоимость билета
+
+int b = 20; // Обменный курс рубль/миля
+
+int result = a / b; // Результат
+
+        System.out.println("Поздравляем! Ваш билет заработал бонусы.");
+        System.out.println("Эти мили для полётов теперь ваши: " + (result) + ".");
+        System.out.println("Приятного путешествия!");
+    }
+}
