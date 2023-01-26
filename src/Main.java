18fd77cd457b0ff6f9009904d114e7c1cee0a792
Artem Khomutskikh <artem7700@mail.ru>
2023-01-26 22:14:30 +0300

    UpdateApp

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticketPrice = 8800; // Стоимость билета
-
-        int exchangeCourse = 20; // Обменный курс рубль/миля
-
-        int result = ticketPrice / exchangeCourse; // Результат
-
-        System.out.println("Поздравляем! Ваш билет заработал бонусы.");
-        System.out.println("Эти мили для полётов теперь ваши: " + (result) + ".");
-        System.out.println("Приятного путешествия!");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println("Получите ваши бонусные мили: " + miles);
     }
 }
